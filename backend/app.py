9223bbee9e7edae408c4e9f60e2f2af057af83da
Igor <igormcsouza@gmail.com>
2020-07-07 22:56:47 -0300

    classification applyed

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,10 @@ import config
 app = Flask(__name__)
 app.config.from_object(config)
 
-@app.route("/")
-def main():
-    return "Hello World"
+from flask_restful import Api
+from resources import MovieReviewPredict
+
+api = Api(app)
+api.add_resource(MovieReviewPredict, '/movie-review-predict')
+
+
